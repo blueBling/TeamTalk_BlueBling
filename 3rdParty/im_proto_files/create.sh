dbd9ae624c647f475cc2a484c27a548f3f86d38c
blueBling <email_qiulei@163.com>
2022-01-21 14:39:27 +0800

    1.增加3rdParty用于存放 log4cxx、protobuf等第三方库 2.encDec加解密模块依赖于utilPdu模块，之前在encDec模块里直接拷贝了utilPdu源文件， 还是依赖外部吧，清爽点 3.增加imPduBase模块，业务相关，主要处理IM消息pdu

---
+++ 3rdParty/im_proto_files/create.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+SRC_DIR=./
+DST_DIR=./gen
+PROTOC=/mnt/hgfs/vmshare_code/teamtalk_blueBling/3rdParty/package_protobuf/bin/protoc
+#C++
+mkdir -p $DST_DIR/cpp
+$PROTOC -I=$SRC_DIR --cpp_out=$DST_DIR/cpp/ $SRC_DIR/*.proto
+
+#JAVA
+mkdir -p $DST_DIR/java
+$PROTOC -I=$SRC_DIR --java_out=$DST_DIR/java/ $SRC_DIR/*.proto
+
+#PYTHON
+mkdir -p $DST_DIR/python
+$PROTOC -I=$SRC_DIR --python_out=$DST_DIR/python/ $SRC_DIR/*.proto
---
+++ 3rdParty/im_proto_files/sync.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+CPP_DIR=../pb/protocol
+DST_DIR=./gen
+
+#C++
+mkdir -p $CPP_DIR
+cp $DST_DIR/cpp/* $CPP_DIR/
+
+#rm -rf ./gen
