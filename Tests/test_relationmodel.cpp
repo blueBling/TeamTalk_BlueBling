eec19912cc5f9def649ca485aeee02e058aa65f8
blueBling <email_qiulei@163.com>
2022-04-06 15:08:57 +0800

    1. 增加RelationModel

---
+++ Tests/test_relationmodel.cpp
@@ -0,0 +1,37 @@
+//
+//  test_relationmodel.cpp
+//  test_relationmodel
+//
+//  Created by blueBling on 22-04-06.
+//  Copyright (c) 2022年blueBling. All rights reserved.
+//
+
+#include "RelationModel.h"
+
+
+#include<iostream>
+
+
+using std::cout;
+using std::endl;
+
+//从mysql IMRelationShip表中获取用户id为1和17关系记录所在id
+int test_getrelationid() {
+
+	uint32_t id = CRelationModel::getInstance()->getRelationId(1, 17, false);
+
+	cout << id << endl;
+
+	return 0;
+}
+
+
+int main(){
+
+	test_getrelationid();
+
+
+	//这里mysql和redis连接池未释放存在内存泄漏问题，解决方法参考test_dbpool
+	
+	return 0;
+}
